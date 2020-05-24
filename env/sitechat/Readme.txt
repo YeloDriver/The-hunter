d1f21841d6df9ba520f2420fcfcf32f3f621af84
VietTien <viet.nguyen@insa-lyon.fr>
2020-05-24 03:33:25 +0200

    update

--- env/sitechat/Readme.txt
+++
@@ -1,6 +0,0 @@
-pip install -r requirements.txt (in a virtualenv preferably)
-(sudo) docker run -p 6379:6379 -d redis:5
-
-python3 manage.py runserver
-
-go to http://127.0.0.1:8000 
--- env/sitechat/chat/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Game(models.Model):
-    session = models.CharField(max_length = 30) # nom de chatroom
-    user = models.CharField(max_length = 30)
-    pos_lng = models.FloatField()
-    pos_lat = models.FloatField()
-    time = models.FloatField()
