303a9feb3f1f83d4027fbd9a68b24c38fcec96fc
VietTien <viet.nguyen@insa-lyon.fr>
2020-05-25 16:41:04 +0200

    admin page

--- sitechat/accounts/admin.py
+++ sitechat/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from chat.models import Game
+from django.contrib.auth.models import Group, User
+from channels_presence.models import Room
+
+class GameAdmin(admin.ModelAdmin):
+    list_display=('session','user','pos_lat','pos_lng','time',)
+    list_filter= ('session',)
+
+admin.site.register(Room)
+admin.site.register(Game,GameAdmin)
+
+admin.site.unregister(Group)
+
+admin.site.site_header = 'Admin Page'
+
+
 
 # Register your models here.
