795190a9e67e828edd903f18bc3d9d87594b26a5
Quentin VITTOZ <quentinvittoz@icloud.com>
2020-05-26 17:27:23 +0200

    fix version mobile

--- sitechat/sitechat/settings.py
+++ sitechat/sitechat/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
         '127.0.0.1',
-        'c62be492.ngrok.io']
+        '301d9fb2.ngrok.io']
 
 
 # Application definition
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels_presence',
-    'accounts'
+    'accounts',
+    'django_user_agents'
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
 ]
 
 ROOT_URLCONF = 'sitechat.urls'
